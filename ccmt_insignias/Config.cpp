2d4d248088bfb6fc97c192297ca72fdd11984d5b
Ashton <ash.wirkijowski@gmail.com>
2018-08-18 21:28:22 +0200

    Create base files

---
+++ ccmt_insignias/Config.cpp
@@ -0,0 +1,31 @@
+/*--------------------------------------------------------+
+|    __________  ___   ________________   ____  ____  ___
+|   / ____/ __ \/   | / ___/_  __/  _/ | / / / / /  |/  /
+|  / /   / /_/ / /| | \__ \ / /  / //  |/ / / / / /|_/ /
+| / /___/ _, _/ ___ |___/ // / _/ // /|  / /_/ / /  / /
+| \____/_/ |_/_/  |_/____//_/ /___/_/ |_/\____/_/  /_/
+|
++---------------------------------------------------------+
+| Made with ❤️ by Crastinum Community's Modding Team
+| http://community.crastinum.eu/
++---------------------------------------------------------+
+| 📧 modding[at]community.crastinum.eu
++------------------------------------------------------//*/
+
+class CfgPatches {
+	class ccmt_insignias {
+		name = "CCMT Insignias";
+		version = "0.1";
+		author[]= {"Crastinum Community Modding Team"};
+		authorUrl = "http://community.crastinum.eu/";
+	};
+};
+
+class CfgUnitInsignia {
+	class ccmt_insignia_1 {
+		displayName = "Crastinum Community";
+		author = "CCMT_SleepyAsh17";
+		texture = "\ccmt_insignias\img\crastinum_community.paa";
+		textureVehicle = "";
+	};
+};
